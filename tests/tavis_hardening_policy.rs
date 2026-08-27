609f1c10864977e8bfd215463e06f3669920d737
WonkaID <61174708+ArtyormSatori@users.noreply.github.com>
2026-08-27 18:19:14 +0700

    style: rustfmt TAVIS hardening test

--- tests/tavis_hardening_policy.rs
+++ tests/tavis_hardening_policy.rs
@@ -2,8 +2,20 @@ use openhuman_core::openhuman::tavis::hardening::{failure_action, FailureAction,
 
 #[test]
 fn critical_failures_have_deterministic_actions() {
-    assert_eq!(failure_action(FailureScenario::OmniRouteUnavailable), FailureAction::DegradeAndRetry);
-    assert_eq!(failure_action(FailureScenario::UpdateHealthCheckFailed), FailureAction::Rollback);
-    assert_eq!(failure_action(FailureScenario::SecurityPolicyTamper), FailureAction::Lockdown);
-    assert_eq!(failure_action(FailureScenario::CoreProcessCrash), FailureAction::RestartWithBackoff);
+    assert_eq!(
+        failure_action(FailureScenario::OmniRouteUnavailable),
+        FailureAction::DegradeAndRetry
+    );
+    assert_eq!(
+        failure_action(FailureScenario::UpdateHealthCheckFailed),
+        FailureAction::Rollback
+    );
+    assert_eq!(
+        failure_action(FailureScenario::SecurityPolicyTamper),
+        FailureAction::Lockdown
+    );
+    assert_eq!(
+        failure_action(FailureScenario::CoreProcessCrash),
+        FailureAction::RestartWithBackoff
+    );
 }
